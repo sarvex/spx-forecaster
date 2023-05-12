22b089fc18db1105e436f7ba1b5e301eebe1138a
Sourcery AI <>
2023-05-12 06:50:08 +0000

    'Refactored by Sourcery'

--- scraper/news-scraper.py
+++ scraper/news-scraper.py
@@ -56,21 +56,17 @@ def get_fxempire(page):
     response = requests.get(url, headers=headers)
     collection = []
 
-    if(response.ok):
+    if response.ok:
         data = response.text
         soup = BeautifulSoup(data, "html.parser")
         articles = soup.select("div.Article-sc-178sudu-0")
-        
+
         for article in articles:
             text = article.get_text()
             text_ = text.split('\n')
-            i = 1
-
-            for title in article.select("a.Link-y81klt-0"):
+            for i, title in enumerate(article.select("a.Link-y81klt-0"), start=2):
                 preproc = title.get_text()
                 preproc_ = preproc.split('\n')[0]
-                i +=1
-
                 if i==2:
                     #headline.append(preproc_)
                     hd = preproc_
@@ -85,13 +81,13 @@ def get_fxempire(page):
                 "date" : dt_
             }
             collection.append(nres)
-            #sleep(2)
+                    #sleep(2)
 
-        #print(collection)
+            #print(collection)
 
     else:
         print(f"No response on page {page}")
-    
+
     return collection
 
 # for CNBC
@@ -101,7 +97,7 @@ def get_cnbc(page):
     response = requests.get(url, headers=headers)
     collection = []
 
-    if(response.ok):
+    if response.ok:
         data = response.text
         soup = BeautifulSoup(data, "html.parser")
         articles = soup.select("#pipeline_assetlist_0")
@@ -109,17 +105,20 @@ def get_cnbc(page):
         for article in articles:
             headlines = article.select("div.headline > a")
             timedata = article.select("time")
-        
+
         for i in range(0,len(headlines)):
             headline = str(headlines[i]).split('>')[1].split('<')[0].strip()
             date = str(timedata[i]).split('>')[1].split('<')[0].strip()
-                    
+
             if "Ago" in date:
                 parsed_s = [date.split()[:2]]
                 parsed_s[0][1] = parsed_s[0][1].lower()
                 if "s" not in parsed_s[0][1]:
                     parsed_s[0][1] += "s"
-                time_dict = dict((TIME_UNIT_LOOKUP.get(fmt,fmt),float(amount)) for amount,fmt in parsed_s)
+                time_dict = {
+                    TIME_UNIT_LOOKUP.get(fmt, fmt): float(amount)
+                    for amount, fmt in parsed_s
+                }
                 dt = datetime.timedelta(**time_dict)
                 past_time = datetime.datetime.now(est) - dt
                 dt_ = past_time.strftime("%Y-%m-%d %H:%M:%S-05:00")
@@ -137,8 +136,7 @@ def get_cnbc(page):
                 if int(hours) < 10:
                     date = "0" + date                    
 
-                if 'Sept' or 'July' or 'June' or 'March' or 'April' in date: # March, April, June, July, and Sept do not come in three-letter abbreviated format
-                    date = date.replace('Sept', 'Sep').replace('July', 'Jul').replace('June', 'Jun').replace('April', 'Apr').replace('March', 'Mar')
+                date = date.replace('Sept', 'Sep').replace('July', 'Jul').replace('June', 'Jun').replace('April', 'Apr').replace('March', 'Mar')
                 dt = datetime.datetime.strptime(date, '%H:%M  %p ET %a,  %d %b %Y')
                 dt_ = dt.strftime("%Y-%m-%d %H:%M:%S-05:00")
                 nres = {
@@ -146,11 +144,11 @@ def get_cnbc(page):
                     "date" : dt_
                 }
                 collection.append(nres)
-                #sleep(2)
+                            #sleep(2)
 
     else:
         print(f"No response on page {page}")
-    
+
     return collection
 
 # for Investing.com
@@ -160,7 +158,7 @@ def get_investing(page):
     response = requests.get(url, headers=headers)
     collection = []
 
-    if(response.ok):
+    if response.ok:
         data = response.text
         soup = BeautifulSoup(data, "html.parser")
         articles = soup.select("div.mediumTitle1 > article.articleItem")
@@ -168,49 +166,43 @@ def get_investing(page):
         for article in articles:
             text = article.get_text()
             text_ = text.strip().split('\n')
-            
+
             to_filter = text_
 
             while("" in to_filter) : 
                 to_filter.remove("")             
-            
+
             if (len(to_filter) > 1):
                 headline = to_filter[0].strip()
                 date = to_filter[1].strip()
                 date_ = re.findall("\w\d\sminute\sago|\w\d\sminutes\sago|\d\shour\sago|\w\d\shours\sago|\d\shours\sago|Jan\s+\d{1,2},\s+\d{4}|Feb\s+\d{1,2},\s+\d{4}|Mar\s+\d{1,2},\s+\d{4}|Apr\s+\d{1,2},\s+\d{4}|May\s+\d{1,2},\s+\d{4}|Jun\s+\d{1,2},\s+\d{4}|Jul\s+\d{1,2},\s+\d{4}|Aug\s+\d{1,2},\s+\d{4}|Sep\s+\d{1,2},\s+\d{4}|Oct\s+\d{1,2},\s+\d{4}|Nov\s+\d{1,2},\s+\d{4}|Dec\s+\d{1,2},\s+\d{4}", date)
                 first_char = date_[0][0] # this is important for backtracking a.k.a. knowing the approximate time stamp for the articles <1 day old
 
-                if(first_char.isdigit()):
+                if (first_char.isdigit()):
                     parsed_s = [date_[0].split()[:2]]
                     if parsed_s[0][1] in ["hour", "minute"]:
                         parsed_s[0][1] += "s" # rare case but the new articles can be a minute or an hour old
-                    time_dict = dict((fmt,float(amount)) for amount,fmt in parsed_s)
+                    time_dict = {fmt: float(amount) for amount,fmt in parsed_s}
                     dt = datetime.timedelta(**time_dict)
                     past_time = datetime.datetime.now(est) - dt
                     dt_ = past_time.strftime("%Y-%m-%d %H:%M:%S-05:00")
 
-                    nres = {
-                        "headline" : headline,
-                        "date" : dt_
-                    }
-                    collection.append(nres)
-                
                 else:
                     dt = datetime.datetime.strptime(date_[0], '%b %d, %Y')
                     dt_ = dt.strftime("%Y-%m-%d %H:%M:%S-05:00")
-                    nres = {
-                        "headline" : headline,
-                        "date" : dt_
-                    }
-                    collection.append(nres)
-                    #sleep(2)
+                                    #sleep(2)
 
-        #print(collection)
-        return collection
-    
+                nres = {
+                    "headline" : headline,
+                    "date" : dt_
+                }
+                collection.append(nres)
+
     else:
         print(f"No response on page {page}")
-        return collection
+
+    #print(collection)
+    return collection
 
 # MAIN
 dfx = "https://www.dailyfx.com/market-news/articles/"
@@ -238,8 +230,8 @@ for p in range(start_p, end_p+1):
 timenow = datetime.datetime.now().strftime("%Y-%m-%d_%H%MH -0800")
 
 pd.DataFrame(collection).to_csv(
-    f'/opt/scr/data/news_{str(start_p)}-{str(end_p)}_{timenow}.zip', 
-    index=False, 
-    columns=['headline', 'date'], 
-    compression='zip'
-    )
+    f'/opt/scr/data/news_{start_p}-{end_p}_{timenow}.zip',
+    index=False,
+    columns=['headline', 'date'],
+    compression='zip',
+)
